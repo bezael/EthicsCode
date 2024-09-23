8b8a541d15469cf85d45744d6fe0f5869d42ecc8
bezael <bezael@gmail.com>
2024-09-23 17:46:01 +0200

    remove comment

--- src/app/features/products/details/details.component.ts
+++ src/app/features/products/details/details.component.ts
@@ -11,8 +11,6 @@ import { selectProductById } from '../state/products.selectors';
   styleUrl: './details.component.scss',
 })
 export default class DetailsComponent {
-  // @Input() productId!: number;
-  // productId = input.required<number>();
   private readonly _store = inject(Store);
   product$ = this._store.selectSignal(selectProductById);
 }
