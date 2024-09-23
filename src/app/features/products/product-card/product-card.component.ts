7c084e8a1c9db649afb9fbe4049edb7aea1b5015
bezael <bezael@gmail.com>
2024-09-23 17:39:44 +0200

    refactor

--- src/app/features/products/product-card/product-card.component.ts
+++ src/app/features/products/product-card/product-card.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  input,
-  Output,
-  output,
-} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../models/product.model';
 
 @Component({
